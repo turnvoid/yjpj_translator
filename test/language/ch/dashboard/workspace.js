9ae24c52b91c0f87c7a15baa9a811dcd226fc92d
陈冠林 <1799285277@qq.com>
2021-05-19 15:48:00 +0800

    完善

---
+++ test/language/ch/dashboard/workspace.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 头部菜单
+  card_title: {
+      processing: '流程中',
+      fastMenu: '快捷导航',
+      dynamic: '最新动态'
+  },
+  card_content: {
+    inspectof: '督学',
+    created: '创建了'
+  }
+}
--- test/language/ch/index.js
+++ test/language/ch/index.js
@@ -1,5 +1,7 @@
+const dashboard = require('./dashboard')
 const statistics = require('./statistics')
 
 module.exports = {
+  dashboard,
   statistics
 }
